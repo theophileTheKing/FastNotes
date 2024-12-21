1e51a8d70483bfe61b1376fb646912afb33e8bae
Theophile Defeche <theophiledefeche@ik.me>
2024-12-21 19:10:08 +0100

    minor

--- obj/Debug/net9.0/FastNotes.AssemblyInfo.cs
+++ obj/Debug/net9.0/FastNotes.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FastNotes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f55a227ee1d1b15a10b429715001ab486c87bbd")]
 [assembly: System.Reflection.AssemblyProductAttribute("FastNotes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FastNotes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
