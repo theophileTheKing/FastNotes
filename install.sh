a843ae19ff32b90935d5c8cc51e85b5b9e928ea3
Theophile Defeche <theophiledefeche@ik.me>
2024-12-25 17:38:49 +0100

    level up the installer script

--- install.sh
+++ install.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 
 # Variables
-APP_URL=https://github.com/theophileTheKing/FastNotes/releases/download/0.1.0/FastNotes
-APP_NAME="FastNotes"
-EXECUTABLE_DIR=/usr/local/bin
+MAIN_URL=https://github.com/theophileTheKing/FastNotes/releases/download/0.1.0/FastNotes
+MAIN_NAME="FastNotes"
+EXECUTABLE_DIR=/opt/fastNotes
+
+SCRIPT_URL=https://raw.githubusercontent.com/theophileTheKing/FastNotes/refs/heads/main/fn
+SCRIPT_NAME="fn"
+SCRIPT_DIR=/usr/local/bin
 
 # Download the app
-echo "Downloading $EXECUTABLE_NAME..."
-curl -L $APP_URL -o $EXECUTABLE_DIR/$APP_NAME
+echo "Downloading $MAIN_NAME..."
+curl -L $MAIN_URL -o $EXECUTABLE_DIR/$MAIN_NAME
+curl -L $SCRIPT_URL -o $SCRIPT_DIR/$SCRIPT_NAME
 
 # Make the app executable
-echo "Making $APP_NAME executable..."
-chmod +x $EXECUTABLE_DIR/$APP_NAME
+echo "Making $MAIN_NAME executable..."
+chmod +x $EXECUTABLE_DIR/$MAIN_NAME
+chmod +x $SCRIPT_DIR/$SCRIPT_NAME
 
 # Confirm installation
-echo "$APP_NAME installed successfully !"
-echo "Type 'FastNotes' to start the app"
+echo "$MAIN_NAME installed successfully !"
+echo "Type 'fn' to start the app"
